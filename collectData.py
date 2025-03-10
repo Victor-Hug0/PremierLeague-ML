c9dfae7dcb8e18cab656c79856489ab35d39828d
Victor-Hug0 <victorhugomourabarreto1@gmail.com>
2025-03-10 17:57:39 -0300

    feat: Extract data from Football-data

---
+++ collectData.py
@@ -0,0 +1,35 @@
+import os
+import pandas as pd
+import requests
+from io import StringIO
+
+base_url = "https://www.football-data.co.uk/mmz4281/{season}/E0.csv"
+seasons = [f"{y-2000:02}{(y+1)-2000:02}" for y in range(2000, 2025)]
+
+os.makedirs("premier_league_data", exist_ok=True)
+
+dataFrames = []
+
+for season in seasons:
+    season_url = base_url.format(season=season)
+    print(f"Temporada {season} -> {season_url}")
+    
+    try:
+        response = requests.get(season_url)
+        response.raise_for_status()
+        
+        dataFrame = pd.read_csv(StringIO(response.text), on_bad_lines="skip")
+        dataFrame["season"] = season
+        dataFrames.append(dataFrame)
+        
+        path = f"premier_league_data/PremierLeague_{season}.csv"
+        dataFrame.to_csv(path, index=False)
+        print(f"Salvo em {path}")
+        
+    except requests.exceptions.RequestException as e:
+        print(f"Erro ao baixar {season_url}: {e}")
+        
+if dataFrames:
+    full_data = pd.concat(dataFrames, ignore_index=True)
+    full_data.to_csv("premier_league_data/PremierLeague_All_Seasons.csv", index=False)
+    print("Todos os dados foram salvos no arquivo: premier_league_data/PremierLeague_All_Seasons.csv")
