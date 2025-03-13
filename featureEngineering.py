3469964898c04417cca851dfefeaf1054c593bcb
Victor-Hug0 <victorhugomourabarreto1@gmail.com>
2025-03-13 16:03:49 -0300

    feat: adding teams ratings and your diffs with adversary team based on Fifa ratings

---
+++ featureEngineering.py
@@ -0,0 +1,71 @@
+import pandas as pd
+import numpy as np
+from loadData import loadDataFrameList
+from teamMapping import standardize_team_names
+
+dfs = loadDataFrameList()
+processed_dfs = []
+
+for df in dfs:
+    teamsRatingFifa = pd.read_csv("TeamsOverInfo/TeamsOverInfo.csv")
+    currentSeason = df["season"].unique()[0]
+    
+    teamsRatingFifa4Season = teamsRatingFifa[teamsRatingFifa["Season"] == currentSeason]
+    
+    df = standardize_team_names(df, team_columns=['HomeTeam', 'AwayTeam'])
+    teamsRatingFifa4Season = standardize_team_names(teamsRatingFifa4Season, team_columns=['Name'])
+    
+    df["OverHome"] = np.nan
+    df["OverAway"] = np.nan
+    df["AttOverHome"] = np.nan
+    df["AttOverAway"] = np.nan
+    df["MidOverHome"] = np.nan
+    df["MidOverAway"] = np.nan
+    df["DefOverHome"] = np.nan
+    df["DefOverAway"] = np.nan
+    
+    teamsSeason = sorted(df["HomeTeam"].unique(), key=str.lower)
+    
+    for i in range(len(teamsSeason)):
+        team = teamsSeason[i]
+        teamGames = df[(df["HomeTeam"] == team) | (df["AwayTeam"] == team)]
+        overTeam = teamsRatingFifa4Season[teamsRatingFifa4Season["Name"] == team]
+    
+        overall = overTeam["Overall"].values[0]
+        attack = overTeam["Attack"].values[0]
+        midfield = overTeam['Midfield'].values[0]
+        defense = overTeam['Defense'].values[0]
+        
+        gameIndex = teamGames.index.tolist()
+        
+        homeGameIndex = []
+        awayGameIndex = []
+        
+        for i, r in teamGames.iterrows():
+            if team == r["HomeTeam"]:
+                homeGameIndex.append(i)
+            elif team == r["AwayTeam"]:
+                awayGameIndex.append(i)
+                
+        for m in range(len(gameIndex)):
+            if gameIndex[m] in homeGameIndex:
+                df.loc[gameIndex[m], "OverHome"] = overall
+                df.loc[gameIndex[m], "AttOverHome"] = attack
+                df.loc[gameIndex[m], "MidOverHome"] = midfield
+                df.loc[gameIndex[m], "DefOverHome"] = defense
+            elif gameIndex[m] in awayGameIndex:
+                df.loc[gameIndex[m], "OverAway"] = overall
+                df.loc[gameIndex[m], "AttOverAway"] = attack
+                df.loc[gameIndex[m], "MidOverAway"] = midfield
+                df.loc[gameIndex[m], "DefOverAway"] = defense
+    
+    
+    df["DiffOverall"] = df.apply(lambda r: r["OverHome"] - r["OverAway"], axis=1)
+    df["DiffAttack"] = df.apply(lambda r: r["AttOverHome"] - r["AttOverAway"], axis=1)
+    df["DiffMidfield"] = df.apply(lambda r: r["MidOverHome"] - r["MidOverAway"], axis=1)
+    df["DiffDefense"] = df.apply(lambda r: r["DefOverHome"] - r["DefOverAway"], axis=1)
+    
+    processed_dfs.append(df)
+
+full_data = pd.concat(processed_dfs, ignore_index=True)
+full_data.to_csv("./fullData.csv", sep=",", index=False)
