1abac4fe20b49368feea6a81ba52d5a38bcc3556
Victor-Hug0 <victorhugomourabarreto1@gmail.com>
2025-03-13 17:47:40 -0300

    feat: adding goals difference

--- featureEngineering.py
+++ featureEngineering.py
@@ -15,6 +15,9 @@ for df in dfs:
     df = standardize_team_names(df, team_columns=['HomeTeam', 'AwayTeam'])
     teamsRatingFifa4Season = standardize_team_names(teamsRatingFifa4Season, team_columns=['Name'])
     
+    df['MHTGD'] = df.apply(lambda row: row['FTHG'] - row['FTAG'], axis=1)
+    df['MATGD'] = df.apply(lambda row: row['FTAG'] - row['FTHG'], axis=1)
+    
     df["OverHome"] = np.nan
     df["OverAway"] = np.nan
     df["AttOverHome"] = np.nan
@@ -23,6 +26,8 @@ for df in dfs:
     df["MidOverAway"] = np.nan
     df["DefOverHome"] = np.nan
     df["DefOverAway"] = np.nan
+    df["GoalsDiffHome"] = np.nan
+    df["GoalsDiffAway"] = np.nan
     
     teamsSeason = sorted(df["HomeTeam"].unique(), key=str.lower)
     
@@ -40,12 +45,20 @@ for df in dfs:
         
         homeGameIndex = []
         awayGameIndex = []
+        goalsDiffPerGame = []
+        goalsDiffCumulative = []
         
         for i, r in teamGames.iterrows():
             if team == r["HomeTeam"]:
                 homeGameIndex.append(i)
+                goalsDiffPerGame.append(r["MHTGD"])
             elif team == r["AwayTeam"]:
                 awayGameIndex.append(i)
+                goalsDiffPerGame.append(r["MATGD"])
+                
+        goalsDiffCumulative = [0]
+        goalsDiffAfterFirstMatch = [sum(goalsDiffPerGame[:i]) for i in range(1, 38)]
+        goalsDiffCumulative += goalsDiffAfterFirstMatch
                 
         for m in range(len(gameIndex)):
             if gameIndex[m] in homeGameIndex:
@@ -53,17 +66,20 @@ for df in dfs:
                 df.loc[gameIndex[m], "AttOverHome"] = attack
                 df.loc[gameIndex[m], "MidOverHome"] = midfield
                 df.loc[gameIndex[m], "DefOverHome"] = defense
+                df.loc[gameIndex[m], "GoalsDiffHome"] = goalsDiffCumulative[m]
             elif gameIndex[m] in awayGameIndex:
                 df.loc[gameIndex[m], "OverAway"] = overall
                 df.loc[gameIndex[m], "AttOverAway"] = attack
                 df.loc[gameIndex[m], "MidOverAway"] = midfield
                 df.loc[gameIndex[m], "DefOverAway"] = defense
+                df.loc[gameIndex[m], "GoalsDiffAway"] = goalsDiffCumulative[m]
     
     
     df["DiffOverall"] = df.apply(lambda r: r["OverHome"] - r["OverAway"], axis=1)
     df["DiffAttack"] = df.apply(lambda r: r["AttOverHome"] - r["AttOverAway"], axis=1)
     df["DiffMidfield"] = df.apply(lambda r: r["MidOverHome"] - r["MidOverAway"], axis=1)
     df["DiffDefense"] = df.apply(lambda r: r["DefOverHome"] - r["DefOverAway"], axis=1)
+    df["GoalsDiff"] = df.apply(lambda r: r["GoalsDiffHome"] - r["GoalsDiffAway"], axis=1)
     
     processed_dfs.append(df)
 
