aa067c7696dd915f85cbb8b0515e303366570b59
Victor-Hug0 <victorhugomourabarreto1@gmail.com>
2025-03-15 21:18:05 -0300

    feat: adding KPP corner, shots on target and goals feature

--- featureEngineering.py
+++ featureEngineering.py
@@ -5,6 +5,7 @@ from teamMapping import standardize_team_names
 
 dfs = loadDataFrameList()
 processed_dfs = []
+k = 6
 
 for df in dfs:
     teamsRatingFifa = pd.read_csv("TeamsOverInfo/TeamsOverInfo.csv")
@@ -18,6 +19,7 @@ for df in dfs:
     df['MHTGD'] = df.apply(lambda row: row['FTHG'] - row['FTAG'], axis=1)
     df['MATGD'] = df.apply(lambda row: row['FTAG'] - row['FTHG'], axis=1)
     
+    #Fifa Rating
     df["OverHome"] = np.nan
     df["OverAway"] = np.nan
     df["AttOverHome"] = np.nan
@@ -26,8 +28,18 @@ for df in dfs:
     df["MidOverAway"] = np.nan
     df["DefOverHome"] = np.nan
     df["DefOverAway"] = np.nan
+    
+    #Goals difference
     df["GoalsDiffHome"] = np.nan
     df["GoalsDiffAway"] = np.nan
+    
+    #KPP (k-Past Performances)
+    df["HomeGoalsKPP"] = np.nan  
+    df["AwayGoalsKPP"] = np.nan  
+    df["HomeCornersKPP"] = np.nan  
+    df["AwayCornersKPP"] = np.nan  
+    df["HomeShotTargetKPP"] = np.nan  
+    df["AwayShotTargetKPP"] = np.nan  
     
     teamsSeason = sorted(df["HomeTeam"].unique(), key=str.lower)
     
@@ -45,20 +57,43 @@ for df in dfs:
         
         homeGameIndex = []
         awayGameIndex = []
+        
+        #Goals difference
         goalsDiffPerGame = []
         goalsDiffCumulative = []
+        
+        #KPP (k-Past Performances)
+        goals = []
+        corners = []
+        shotsTarget = []
+        goalsKPP = [np.nan] * k
+        cornersKPP = [np.nan] * k
+        shotsTargetKPP = [np.nan] * k
+        
         
         for i, r in teamGames.iterrows():
             if team == r["HomeTeam"]:
                 homeGameIndex.append(i)
                 goalsDiffPerGame.append(r["MHTGD"])
+                goals.append(r["FTHG"])
+                corners.append(r["HC"])
+                shotsTarget.append(r["HST"])
             elif team == r["AwayTeam"]:
                 awayGameIndex.append(i)
                 goalsDiffPerGame.append(r["MATGD"])
+                goals.append(r["FTAG"])
+                corners.append(r["AC"])
+                shotsTarget.append(r["AST"])
                 
         goalsDiffCumulative = [0]
-        goalsDiffAfterFirstMatch = [sum(goalsDiffPerGame[:i]) for i in range(1, len(gameIndex)+1)]
+        goalsDiffAfterFirstMatch = [sum(goalsDiffPerGame[:i]) for i in range(1, len(gameIndex))]
         goalsDiffCumulative += goalsDiffAfterFirstMatch
+        
+        for i in range(k, len(gameIndex)):
+            goalsKPP.append(round(sum(goals[i:i+k]) / k, 2))
+            cornersKPP.append(round(sum(corners[i:i+k]) / k, 2))
+            shotsTargetKPP.append(round(sum(shotsTarget[i:i+k]) / k, 2))
+
                 
         for m in range(len(gameIndex)):
             if gameIndex[m] in homeGameIndex:
@@ -67,19 +102,31 @@ for df in dfs:
                 df.loc[gameIndex[m], "MidOverHome"] = midfield
                 df.loc[gameIndex[m], "DefOverHome"] = defense
                 df.loc[gameIndex[m], "GoalsDiffHome"] = goalsDiffCumulative[m]
+                df.loc[gameIndex[m], "HomeGoalsKPP"] = goalsKPP[m]
+                df.loc[gameIndex[m], "HomeCornersKPP"] = cornersKPP[m]
+                df.loc[gameIndex[m], "HomeShotTargetKPP"] = shotsTargetKPP[m]
             elif gameIndex[m] in awayGameIndex:
                 df.loc[gameIndex[m], "OverAway"] = overall
                 df.loc[gameIndex[m], "AttOverAway"] = attack
                 df.loc[gameIndex[m], "MidOverAway"] = midfield
                 df.loc[gameIndex[m], "DefOverAway"] = defense
                 df.loc[gameIndex[m], "GoalsDiffAway"] = goalsDiffCumulative[m]
+                df.loc[gameIndex[m], "AwayGoalsKPP"] = goalsKPP[m]
+                df.loc[gameIndex[m], "AwayCornersKPP"] = cornersKPP[m]
+                df.loc[gameIndex[m], "AwayShotTargetKPP"] = shotsTargetKPP[m]
     
     
+    #Fifa Rating
     df["DiffOverall"] = df.apply(lambda r: r["OverHome"] - r["OverAway"], axis=1)
     df["DiffAttack"] = df.apply(lambda r: r["AttOverHome"] - r["AttOverAway"], axis=1)
     df["DiffMidfield"] = df.apply(lambda r: r["MidOverHome"] - r["MidOverAway"], axis=1)
     df["DiffDefense"] = df.apply(lambda r: r["DefOverHome"] - r["DefOverAway"], axis=1)
+    #Goals difference
     df["GoalsDiff"] = df.apply(lambda r: r["GoalsDiffHome"] - r["GoalsDiffAway"], axis=1)
+    #KPP (k-Past Performances)
+    df["GoalsKPP"] = df.apply(lambda r: r["HomeGoalsKPP"] - r["AwayGoalsKPP"], axis=1)
+    df["CornersKPP"] = df.apply(lambda r: r["HomeCornersKPP"] - r["AwayCornersKPP"], axis=1)
+    df["ShotsTargetKPP"] = df.apply(lambda r: r["HomeShotTargetKPP"] - r["AwayShotTargetKPP"], axis=1)
     
     processed_dfs.append(df)
 
